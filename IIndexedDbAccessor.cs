2908f888a703140c7085e7aef89e8657c6f96994
Dylan Cook <dcook@email.neit.edu>
2023-08-07 17:42:40 -0400

    State saves

--- IIndexedDbAccessor.cs
+++
@@ -1,6 +0,0 @@
-﻿public interface IIndexedDbAccessor : IAsyncDisposable
-{
-    Task<T> GetValueAsync<T>(string collectionName, int id);
-    Task InitializeAsync();
-    Task SetValueAsync<T>(string collectionName, T value);
-}
