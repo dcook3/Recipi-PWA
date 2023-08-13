4ad9c3b74056700bab2bfe18997f3b517455a1d8
Justin <jdramos1@email.neit.edu>
2023-08-13 18:47:29 -0400

    Added frontend logic to upload utilizing the presigned urls from our api. Call this js function with a file to upload it eg. (<input type='file' onchange'fileUpload(even.target.files[0])'>

---
+++ wwwroot/js/UploadToPresignedUrl.js
@@ -0,0 +1,16 @@
+﻿//Takes a file and uploads it to an S3 Bucket with a presigned url. Respond with a link pointing to the file stored in the bucket.
+async function fileUpload(file) {
+    const url = await fetch("/api/MediaUpload").then(response => response.text());
+    console.log(file);
+    // post the image direclty to the s3 bucket
+    await fetch(url, {
+        method: "PUT",
+        headers: {
+            "Content-Type": file.type,
+        },
+        body: file
+    });
+
+    const imageUrl = url.split('?')[0];
+    return imageUrl;
+}
