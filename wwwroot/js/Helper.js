c21732f10fcb855b07e9cec05950162e18d4262a
Lucas Johnson <lucas.johnson02@outlook.com>
2023-08-20 16:30:02 -0400

    want to test on mobile again

--- wwwroot/js/Helper.js
+++ wwwroot/js/Helper.js
@@ -11,18 +11,24 @@ var preview;
 var result;
 var ready;
 var recordedChunks = [];
-const recording = false;
+var recording = false;
 
-window.InitializeVideo = () => {
+var devices;
+var deviceIndex = -1;
+
+window.InitializeVideo = async () => {
     
     preview = document.getElementById("preview");
     result = document.getElementById("result");
     ready = document.getElementById("ready");
-    preview.width = window.innerWidth;
-    preview.height = window.innerHeight;
-    result.width = window.innerWidth;
-    result.height = window.innerHeight;
-    navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+    //preview.width = window.innerWidth;
+    //preview.height = window.innerHeight;
+    //result.width = window.innerWidth;
+    //result.height = window.innerHeight;
+   
+    devices = await navigator.mediaDevices.enumerateDevices();
+    getNextCamera();
+    navigator.mediaDevices.getUserMedia({ video: { deviceId: { exact: devices[deviceIndex].deviceId } }, audio: true })
         .then(stream => {
             // Display webcam in preview video element
             preview.srcObject = stream;
@@ -31,49 +37,87 @@ window.InitializeVideo = () => {
             preview.captureStream = preview.captureStream || preview.mozCaptureStream;
 
             // Wait until the preview is visible before recording
-            return new Promise(resolve => preview.onplaying = resolve);
+            //return new Promise(resolve => preview.onplaying = resolve);
         })
 
    
 
 }
 
-window.StartRecording = () => {
-    navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-        .then(stream => {
-            // Display webcam in preview video element
-            preview.srcObject = stream;
+var getNextCamera = () => {
+    console.log(devices[deviceIndex])
+    do {
+        deviceIndex++;
+        if(deviceIndex >= devices.length) {
+            deviceIndex = 0;
+        }
+    } while (devices[deviceIndex].kind != "videoinput")
+    console.log(devices[deviceIndex])
+}
 
-            // Handle browser compatibility issues
-            preview.captureStream = preview.captureStream || preview.mozCaptureStream;
+window.NextCamera = () => {
+    if (!recording) {
+        getNextCamera();
+        navigator.mediaDevices.getUserMedia({ video: { deviceId: { exact: devices[deviceIndex].deviceId } }, audio: true })
+            .then(stream => {
+                // Display webcam in preview video element
+                preview.srcObject = stream;
 
-            // Wait until the preview is visible before recording
-            return new Promise(resolve => preview.onplaying = resolve);
-        })
-        .then(() => {
+                // Handle browser compatibility issues
+                preview.captureStream = preview.captureStream || preview.mozCaptureStream;
+
+                // Wait until the preview is visible before recording
+                //return new Promise(resolve => preview.onplaying = resolve);
+            })
+    }
+}
+
+window.StartRecording = async () => {
+    if (!recording) {
+        recording = true;
+        return await new Promise((success, error) => {
+    
             recordedChunks = [];
             const stream = preview.captureStream();
             recorder = new MediaRecorder(stream);
-            recorder.ondataavailable = event => {
-                    recordedChunks.push(event.data)
+
+            recorder.ondataavailable = e => {
+                if (e.data.size > 0) {
+                    recordedChunks.push(e.data)
+                }
             };
+
+            recorder.onstop = async () => {
+                recording = false;
+                const blob = new Blob(recordedChunks, { type: 'video/webm' });
+                const content = await (new Response(blob).arrayBuffer());
+                const contentNums = new Uint8Array(content);
+                const contentCodes = new TextDecoder('windows-1252').decode(contentNums);
+
+
+
+                result.src = URL.createObjectURL(blob);
+                
+
+                
+                success(contentCodes)
+            }
+
             recorder.start();
+            setTimeout(() => { if (recording) { recorder.stop() } }, 7000);
         })
+    }
 }
 
 window.StopRecording = () => {
-    
-
     recorder.stop();
+}
 
-    recorder.onstop = () => {
-        const blob = new Blob(recordedChunks, { type: 'video/webm' });
-
-        result.src = URL.createObjectURL(blob);
-        ready.click();
+window.TogglePlayback = () => {
+    if (result.paused) {
+        result.play();
     }
-
-    
-    
-
+    else {
+        result.pause();
+    }
 }
