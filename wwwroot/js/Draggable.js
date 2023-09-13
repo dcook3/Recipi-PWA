dd02d9ca18d0aa0c5e104a00b288dcc2548af51b
Lucas Johnson <lucas.johnson02@outlook.com>
2023-09-13 15:15:59 -0400

    post view and feeds should be working

--- wwwroot/js/Draggable.js
+++ wwwroot/js/Draggable.js
@@ -103,3 +103,143 @@ window.InitDraggable = async (id) => {
     //}
 }
 
+var posts;
+var currentIndex = 0; 
+
+var initSinglePostSwipe = async (post, obj) => {
+    var wrapper = post.parentElement;
+    var video = post.querySelector("video");
+    console.log(video);
+    if (video) {
+        var handleMouseDown = async (e) => {
+            console.log("hello")
+            if (e.pageY) {
+
+                var ogMousePX = e.pageY;
+            }
+            else {
+                var ogMousePX = e.touches[0].pageY;
+            }
+            var lastMousePos = ogMousePX
+            wrapper.style.transitionDuration = "0s";
+            var setPostPos = async (direction = "stay") => {
+
+                switch (direction) {
+                    case "up":
+                        if (currentIndex > 1) {
+                            wrapper.prepend(wrapper.lastElementChild);
+                            wrapper.style.transitionDuration = "0ms";
+                            wrapper.style.top = (parseInt(wrapper.style.top, 10) - post.offsetHeight) + "px";
+                            currentIndex = await obj.invokeMethodAsync("NextPost", "up", parseInt(wrapper.firstElementChild.id, 10))
+                            wrapper.lastElementChild.querySelector("video").pause();
+                            wrapper.children[1].querySelector("video").play();
+                        }
+                        else if (currentIndex == 1) {
+                            currentIndex = await obj.invokeMethodAsync("NextPost", "up", parseInt(wrapper.firstElementChild.id, 10));
+                            wrapper.firstElementChild.querySelector("video").play();
+                            wrapper.children[1].querySelector("video").pause();
+                        }
+                        break;
+                    case "down":
+
+                        if (currentIndex == 0) {
+                            currentIndex = await obj.invokeMethodAsync("NextPost", "down", parseInt(wrapper.lastElementChild.id, 10))
+                            wrapper.firstElementChild.querySelector("video").pause();
+                            wrapper.children[1].querySelector("video").play();
+                        }
+
+                        else {
+
+
+                            wrapper.appendChild(wrapper.firstElementChild);
+                            wrapper.style.top = (parseInt(wrapper.style.top, 10) + post.offsetHeight) + "px";
+                            wrapper.firstElementChild.querySelector("video").pause();
+                            wrapper.children[1].querySelector("video").play();
+                            currentIndex = await obj.invokeMethodAsync("NextPost", "down", parseInt(wrapper.lastElementChild.id, 10))
+                        }
+
+
+                        break;
+                }
+                console.log(currentIndex)
+                if (currentIndex == 0) {
+                    await sleep(1);
+                    wrapper.style.transitionDuration = "500ms";
+                    wrapper.style.top = "0px";
+                    await sleep(500);
+                    wrapper.style.transitionDuration = "0ms";
+                }
+                else {
+                    await sleep(1);
+                    wrapper.style.transitionDuration = "500ms";
+                    wrapper.style.top = -(post.offsetHeight) + "px";
+                    await sleep(500);
+                    wrapper.style.transitionDuration = "0ms";
+                }
+            }
+
+            var handleMouseMove = (e) => {
+                if (e.pageY) {
+
+                    var py = e.pageY;
+                }
+                else {
+                    var py = e.touches[0].pageY;
+                }
+                var mouseDiff = py - lastMousePos;
+                lastMousePos = py;
+
+                if (Math.abs(py - ogMousePX) < 200) {
+                    wrapper.style.top = parseInt(wrapper.style.top, 10) + mouseDiff + "px";
+                }
+                else {
+                    if (py < ogMousePX) {
+                        setPostPos("down");
+                    }
+                    else {
+                        setPostPos("up");
+                    }
+                    video.removeEventListener("mousemove", handleMouseMove)
+                    video.removeEventListener("touchmove", handleMouseMove)
+                    window.removeEventListener("mouseup", (e) => {
+                        video.removeEventListener("mousemove", handleMouseMove)
+                        video.removeEventListener("touchmove", handleMouseMove)
+                        setPostPos();
+                    })
+                    window.removeEventListener("touchend", (e) => {
+                        video.removeEventListener("mousemove", handleMouseMove)
+                        video.removeEventListener("touchmove", handleMouseMove)
+                        setPostPos();
+                    })
+                }
+            }
+
+            video.addEventListener("mousemove", handleMouseMove)
+            video.addEventListener("touchmove", handleMouseMove)
+            window.addEventListener("mouseup", (e) => {
+                video.removeEventListener("mousemove", handleMouseMove)
+                video.removeEventListener("touchmove", handleMouseMove)
+                setPostPos();
+            })
+            window.addEventListener("touchend", (e) => {
+                video.removeEventListener("mousemove", handleMouseMove)
+                video.removeEventListener("touchmove", handleMouseMove)
+                setPostPos();
+            })
+        }
+
+        video.addEventListener("mousedown", handleMouseDown)
+        video.addEventListener("touchstart", handleMouseDown)
+    }
+}
+
+window.InitPostsSwipe = async (obj) => {
+    posts = document.querySelectorAll(".post-wrapper");
+    
+    posts.forEach((post) => initSinglePostSwipe(post, obj))
+}
+
+window.InitPostSwipe = async (id, obj) => {
+    post = document.getElementById(id);
+    initSinglePostSwipe(post, obj)
+}
